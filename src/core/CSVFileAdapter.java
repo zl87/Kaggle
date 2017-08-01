9fb5a6871a308ca18bb82171be4a798b8d11335f
Jacob and Jessie <Jacob and Jessie@192.168.0.13>
2017-07-31 23:43:01 -0400

    Added csv file adapter.

---
+++ src/core/CSVFileAdapter.java
@@ -0,0 +1,15 @@
+package core;
+
+import java.io.Reader;
+
+import com.opencsv.CSVReader;
+
+public class CSVFileAdapter extends CSVReader
+{
+
+   public CSVFileAdapter(Reader reader)
+   {
+      super(reader);
+      // TODO Auto-generated constructor stub
+   }
+}
--- src/core/Main.java
+++ src/core/Main.java
@@ -3,6 +3,10 @@ package core;
 public class Main
 {
 
+   private final String sTestFilePath = "";
+
+   private final String sTrainFilePath = "";
+
    public static void main(String[] args)
    {
       // TODO Auto-generated method stub
